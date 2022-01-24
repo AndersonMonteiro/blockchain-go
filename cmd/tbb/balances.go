8695e2e03d20ecd5d1814a6c941fc0f05a4dfcb6
AndersonMonteiro <anderson_mont@yahoo.com.br>
2022-01-24 18:05:01 -0300

    set c3 class

---
+++ cmd/tbb/balances.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/AndersonMonteiro/blobkchain-go/database"
+	"github.com/spf13/cobra"
+)
+
+func balancesCmd() *cobra.Command {
+	var balancesCmd = &cobra.Command{
+		Use:   "balances",
+		Short: "Interact with balances (list...).",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return incorrectUsageErr()
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+
+	balancesCmd.AddCommand(balancesListCmd)
+
+	return balancesCmd
+}
+
+var balancesListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "Lists all balances.",
+	Run: func(cmd *cobra.Command, args []string) {
+		state, err := database.NewStateFromDisk()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer state.Close()
+
+		fmt.Println("Accounts balances:")
+		fmt.Println("__________________")
+		fmt.Println("")
+		for account, balance := range state.Balances {
+			fmt.Println(fmt.Sprintf("%s: %d", account, balance))
+		}
+	},
+}
